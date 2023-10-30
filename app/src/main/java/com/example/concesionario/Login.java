dc03f32942c58e4f15c0cbac2a85e22edd00f469
Andres Gongora <agongoraramirez@gmail.com>
2023-10-29 23:10:15 -0500

    Ya se cuenta con la vista principal, login y parcialmente la "vista" de registro

--- app/src/main/java/com/example/concesionario/Login.java
+++ app/src/main/java/com/example/concesionario/Login.java
@@ -1,21 +1,43 @@
 package com.example.concesionario;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class Login extends AppCompatActivity {
-    EditText Name, Direccion, Password, PasswordC;
+    CardView tarjeta;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch switch1;
+    LinearLayout contenidoOriginal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Name = findViewById(R.id.Name);
-        Direccion = findViewById(R.id.Direccion);
-        Password = findViewById(R.id.Password);
-        PasswordC = findViewById(R.id.PasswordC);
+
+        tarjeta = findViewById(R.id.Caja);
+        switch1 = findViewById(R.id.switch1);
+        contenidoOriginal = findViewById(R.id.contenidoOriginal); // Asegúrate de asignar un ID a tu LinearLayout original
+
+        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Cambiar el contenido de la CardView según el estado del Switch
+            if (isChecked) {
+                // Mostrar contenido cuando el Switch está activado
+                // Inflar el nuevo contenido desde nuevo_contenido.xml
+                View caja_registro = LayoutInflater.from(Login.this).inflate(R.layout.caja_registro, tarjeta, false);
+                tarjeta.removeAllViews(); // Eliminar el contenido anterior
+                tarjeta.addView(caja_registro); // Establecer el nuevo contenido
+            } else {
+                // Cambiar el contenido cuando el Switch está desactivado
+                // Restaurar el contenido original
+                tarjeta.removeAllViews(); // Eliminar el contenido anterior
+                tarjeta.addView(contenidoOriginal); // Restaurar el contenido original
+            }
+        });
     }
-
-
 }
